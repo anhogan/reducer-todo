49db3ffe488b9af0ea24fc8f187bf9aa097abdac
Amanda Hogan <ahogan@teamworks.com>
2020-02-16 20:10:44 -0500

    Setup component files

---
+++ todo-list/src/components/ToDoForm.js
@@ -0,0 +1,11 @@
+import React, { useReducer } from 'react';
+
+const ToDoForm = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ToDoForm;
---
+++ todo-list/src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from 'react';
+import ToDoForm from './ToDoForm';
+
+const ToDoList = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ToDoList;
